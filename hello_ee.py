aa756ed96f19b832cad18ba09ab40ec73c32dcb0
neriiacopo <neri.iacopo@gmail.com>
2022-04-08 19:56:51 +0200

    version 2

--- hello_ee.py
+++
@@ -1,7 +0,0 @@
-import ee
-
-# Autheticate to Earth Engine via browser to store locally User and Key
-ee.Authenticate()
-
-# Test the success of the process by connecting to Earth Engine
-ee.Initialize()
--- import_ee.py
+++ import_ee.py
@@ -1,24 +1,75 @@
 from flask import Flask
 import ghhops_server as hs
-import pandas as pd
 import numpy as np
 import ee
-import geemap
 import os
 from pyproj import CRS
 from pyproj.aoi import AreaOfInterest
 from pyproj.database import query_utm_crs_info
 from pyproj import Transformer
 
-# Make to first run hello_ee.py to do the one-time authentication to Earth Engine
+# ---------------------------------------------------------------------------------------------------------------
+#                 First time use will open a webpage to authenticate the machine locally 
+# ---------------------------------------------------------------------------------------------------------------
+
+# App Setup -----------------------------------------------------------------------------------------------------
 
 # register hops app as middleware
 app = Flask(__name__)
 hops: hs.HopsFlask = hs.Hops(app)
 
+# Initialize earth engine
+try:
+    ee.Initialize()
+except Exception as e:
+    ee.Authenticate()
+    ee.Initialize()
 
-# Initial earth engine
-ee.Initialize()
+# Global Functions ----------------------------------------------------------------------------------------------
+
+def pts_bbox(pts):
+    pts_py = []
+    for p in pts: 
+        pts_py.append([p.X, p.Y])
+
+    aoi = ee.Geometry.Polygon(
+        [[[pts_py[0][0], pts_py[0][1]],
+        [pts_py[1][0], pts_py[1][1]],
+        [pts_py[2][0], pts_py[2][1]],
+        [pts_py[3][0], pts_py[3][1]]]], None, False)
+
+    return aoi;
+
+def img_scaleTrim(image, band, proj, scale, pts):
+    
+    # Resample the image to assign custom scale
+    imageResampled = image \
+        .reduceResolution(**{
+        'reducer': ee.Reducer.mean(),
+        'maxPixels': 5000
+        }) \
+        .reproject(**{
+        'crs': proj,
+        'scale': scale
+        })
+
+    # Extract the bounding region from the locations
+    aoi = pts_bbox(pts)
+
+    # Sample the image within the region
+    rec_sample = imageResampled.sampleRectangle(region=aoi, defaultValue=0)
+
+    # Prepare the outputs
+    rgb_img = np.array(rec_sample.get(band).getInfo())
+
+    # Change from pixel to node (vertices)
+    H = rgb_img.shape[0] -1
+    W = rgb_img.shape[1] -1
+    
+    return rgb_img.flatten().tolist(),W,H,;
+
+
+# App Components -----------------------------------------------------------------------------------------------
 
 @hops.component(
     "/ee_image",
@@ -36,49 +87,14 @@ ee.Initialize()
 
 def ee_image(layer,bands,scale,pts):
 
-    print(type(layer))
-    print(bands)
-    # Create a map to upload the layers to.
-    Map = geemap.Map()
-
-    # Add image layer
+    # Select image layer
     image = ee.Image(layer)\
         .select(bands) 
 
-    layer_viz = {
-        'bands': [bands]
-    }
-    Map.addLayer(image, layer_viz, None, False, 1)
+    # Extract original projection
+    proj = image.projection()
 
-    imageProjection = image.projection()
-
-    # Resample the image to assign custom scale
-    imageResampled = image \
-        .reduceResolution(**{
-        'reducer': ee.Reducer.mean(),
-        'maxPixels': 5000
-        }) \
-        .reproject(**{
-        'crs': imageProjection,
-        'scale': scale
-        })
-
-    pts_py = []
-    print(pts)
-    for p in pts: 
-        pts_py.append([p.X, p.Y])
-
-    aoi = ee.Geometry.Polygon(
-        [[[pts_py[0][0], pts_py[0][1]],
-        [pts_py[1][0], pts_py[1][1]],
-        [pts_py[2][0], pts_py[2][1]],
-        [pts_py[3][0], pts_py[3][1]]]], None, False)
-
-    rgb_img = geemap.ee_to_numpy(imageResampled, default_value=0, region=aoi)
-    H = rgb_img.shape[0] 
-    W = rgb_img.shape[1] 
-    
-    return rgb_img.flatten().tolist(),W,H,;
+    return img_scaleTrim(image, bands, proj, scale, pts);
 
 
 @hops.component(
@@ -88,7 +104,7 @@ def ee_image(layer,bands,scale,pts):
         hs.HopsString("bands", "bands"),
         hs.HopsString("date", "date", "input date", hs.HopsParamAccess.LIST),
         hs.HopsNumber("scale", "scale"),
-        hs.HopsPoint("bounding box","bbox","the bounding box representing the area of analaysis. Note, provide it in the following order: min.Lon(X), max.Lon(X), min.Lat(Y), max.Lat(Y) aka LeftBottom, RightBottom, RightTop, LeftTop", hs.HopsParamAccess.LIST)],
+        hs.HopsPoint("bounding box","bbox","the bounding box representing the area of analysis. Note, provide it in the following order: min.Lon(X), max.Lon(X), min.Lat(Y), max.Lat(Y) aka LeftBottom, RightBottom, RightTop, LeftTop", hs.HopsParamAccess.LIST)],
     outputs=[
         hs.HopsNumber("values"),
         hs.HopsNumber("W"),
@@ -98,55 +114,24 @@ def ee_image(layer,bands,scale,pts):
 
 def ee_imageCollection(layer,bands,date,scale,pts):    
 
-    # Create a map to upload the layers to.
-    Map = geemap.Map()
+    # Extract the bounding region from the locations
+    aoi = pts_bbox(pts)
 
-    # Create region for spatial filter
-    pts_py = []
-    print(pts)
-    for p in pts: 
-        pts_py.append([p.X, p.Y])
-
-    aoi = ee.Geometry.Polygon(
-        [[[pts_py[0][0], pts_py[0][1]],
-        [pts_py[1][0], pts_py[1][1]],
-        [pts_py[2][0], pts_py[2][1]],
-        [pts_py[3][0], pts_py[3][1]]]], None, False)
-
-    # Add image layer and filter on Dates
+    # Select image layer and filter on Region and Dates
     imageCollection = ee.ImageCollection(layer)\
                     .select(bands)\
                     .filterBounds(aoi)\
                     .filterDate(date[0],date[1])\
                     .limit(10)
 
-    imageProjection = imageCollection.first().projection()
+    # Extract original projection
+    proj = imageCollection.first().projection()
 
+    # Create a mosaic from available collection
     imageMosaic = ee.Image(imageCollection.mosaic())\
                             .setDefaultProjection(imageProjection)
 
-    layer_viz = {
-        'bands': [bands]
-    }
-
-    Map.addLayer(imageMosaic, layer_viz, None, False, 1)
-
-    # Resample the image to assign custom scale
-    imageResampled = imageMosaic \
-        .reduceResolution(**{
-        'reducer': ee.Reducer.mean(),
-        'maxPixels': 5000
-        }) \
-        .reproject(**{
-        'crs': imageProjection,
-        'scale': scale
-        })
-
-    rgb_img = geemap.ee_to_numpy(imageResampled, default_value=0, region=aoi)
-    H = rgb_img.shape[0] 
-    W = rgb_img.shape[1] 
-    
-    return rgb_img.flatten().tolist(),W,H,;
+    return img_scaleTrim(imageMosaic, bands, proj, scale, pts);
 
 @hops.component(
     "/ee_nd",
@@ -165,47 +150,76 @@ def ee_imageCollection(layer,bands,date,scale,pts):
 
 def ee_ND(layer,band1,band2,scale,pts):
 
-    Map = geemap.Map()
-    # Add Earth Engine dataset
-    landsat = ee.Image(layer)
+    # Select the two bands to subtract
+    image1 = ee.Image(layer).select(band1)
+    image2 = ee.Image(layer).select(band2)
 
-    # Compute NDVI the easy way.
-    ndvi1999 = landsat.normalizedDifference([band1, band2])
+    # Compute Normalized Difference
+    imageND = image1.subtract(image2) \
+              .divide(image1.add(image2)) \
+              .rename("ND")
 
-    # Add image layer
-    image = ndvi1999 
+    # Extract original projection
+    proj = image1.projection()
 
-    Map.addLayer(image, {}, None, False, 1)
-    imageProjection = image.projection()
+    return img_scaleTrim(imageND, "ND", proj, scale, pts)
+    
+@hops.component(
+    "/ee_cumCost",
+    inputs=[       
+        hs.HopsString("layer", "layer", "the layer from which to calculate the cost to traverse"),
+        hs.HopsString("cost", "cost"),        
+        hs.HopsPoint("sources", "sources", "the location to where to calculate the proximity", hs.HopsParamAccess.LIST),
+        hs.HopsNumber("maxdistance", "maxd"),
+        hs.HopsNumber("scale", "scale"),
+        hs.HopsPoint("bounding box","bbox","the bounding box representing the area of analaysis. Note, provide it in the following order: min.Lon(X), max.Lon(X), min.Lat(Y), max.Lat(Y) aka LeftBottom, RightBottom, RightTop, LeftTop", hs.HopsParamAccess.LIST)],
+    outputs=[
+        hs.HopsNumber("values"),
+        hs.HopsNumber("W"),
+        hs.HopsNumber("H")
+    ],
+)
 
-    # Resample the image to assign custom scale
-    imageResampled = image \
+def ee_cumCost(layer,cost,sources,maxd,scale,pts):
+
+    # Compute the centroid location
+    coords = []
+    for p in sources: 
+
+        coord = [p.X,p.Y]
+        coords.append(coord)
+
+    geom_source = ee.Geometry.MultiPoint(coords)
+
+    # Rasterize location into an image where the geometry is 1, everything else is 0
+    imageSources = ee.Image().toByte().paint(geom_source, 1)
+
+    # Mask the sources image with itself.
+    imageSources = imageSources.updateMask(imageSources)
+
+    # Select cost data
+    cost = ee.Image(layer)\
+        .select(cost) 
+
+    # Extract original projection
+    proj = cost.projection()
+
+    # Resample resolution
+    costResample = cost \
         .reduceResolution(**{
-        'reducer': ee.Reducer.mode(),
+        'reducer': ee.Reducer.mean(),
         'maxPixels': 5000
         }) \
         .reproject(**{
-        'crs': imageProjection,
+        'crs': proj,
         'scale': scale
         })
 
-    pts_py = []
-    print(pts)
-    for p in pts: 
-        pts_py.append([p.X, p.Y])
+    # Compute the cumulative cost
+    cumulativeCost = costResample.cumulativeCost(source=imageSources, maxDistance=ee.Number.float(maxd)) \
+        .rename("cumcost")
 
-    aoi = ee.Geometry.Polygon(
-        [[[pts_py[0][0], pts_py[0][1]],
-        [pts_py[1][0], pts_py[1][1]],
-        [pts_py[2][0], pts_py[2][1]],
-        [pts_py[3][0], pts_py[3][1]]]], None, False)
-
-    # rgb is a three dimension array (firt two being the data and third being relative to the band)
-    rgb_img = geemap.ee_to_numpy(imageResampled, default_value=0, region=aoi)
-    H = rgb_img.shape[0] 
-    W = rgb_img.shape[1] 
-    
-    return rgb_img.flatten().tolist(),W,H,;
+    return img_scaleTrim(ee.Image(cumulativeCost), "cumcost", proj, scale, pts)
 
 @hops.component(
     "/reproject_UTM",
@@ -222,8 +236,7 @@ def ee_ND(layer,band1,band2,scale,pts):
 
 def reproject_UTM(pts):
 
-    
-    # Extract the average location
+    # Compute the centroid location
     xs = []
     ys = []
 
@@ -232,7 +245,8 @@ def reproject_UTM(pts):
         ys.append(p.Y)
 
     p_mean = [np.asarray(xs).mean(),np.asarray(ys).mean()]
-    
+
+    # Extract UTM of the centroid location    
     utm_crs_list = query_utm_crs_info(
         datum_name="WGS 84",
         area_of_interest=AreaOfInterest(
@@ -256,9 +270,10 @@ def reproject_UTM(pts):
         p_UTM = transformer.transform(p.X,p.Y)
         pts_UTM.append(str("{" + str(p_UTM[0]) +"," + str(p_UTM[1]) + ",0}"))
 
-    print(pts_UTM)
     return pts_UTM;
 
-# Run app
+
+# Run App ------------------------------------------------------------------------------------------------------
+
 if __name__ == "__main__":
     app.run(debug=True)
